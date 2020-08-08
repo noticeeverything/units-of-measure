80c1f8db3fda4c43ea981c6ac407f3824eae7e52
Justin McMahon <justin@justinmcmahon.com>
2020-08-08 07:56:23 -0700

    Move header from converter to app and add app component specs

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -29,4 +29,24 @@ describe('AppComponent', () => {
   it('creates', () => {
     expect(component).toBeTruthy();
   });
+
+  it('has a footer component', () => {
+    expect(spectator.query(FooterComponent)).toBeTruthy();
+  });
+
+  it('has a router outlet', () => {
+    expect(spectator.query(RouterOutlet)).toBeTruthy();
+  });
+
+  it('has a title', () => {
+    expect(spectator.query('h1')).toHaveExactText('Easy Unit Converter');
+  });
+
+  it('has a byline', () => {
+    expect(spectator.query('p.lead')).toHaveExactText('by Notice Everything Creative');
+  });
+
+  it('links to our website', () => {
+    expect(spectator.query('.container a')).toContainProperty('href', 'https://noticeeverything.com');
+  });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,13 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'uom-root',
   template: `
-        <router-outlet></router-outlet>
-		<uom-footer></uom-footer>
-	`
+    <div class="container pt-5 text-center">
+      <h1>Easy Unit Converter</h1>
+      <p class="lead">by <a href="https://noticeeverything.com" rel="noopener" target="_blank">Notice Everything Creative</a></p>
+      <router-outlet></router-outlet>
+    </div>
+    <uom-footer></uom-footer>
+  `
 })
 export class AppComponent {
 
